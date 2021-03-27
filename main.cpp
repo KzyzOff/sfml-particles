48c6aba5089279137dfe9281f274fec71cf9c488
Krzysztof Dybus <253340@student.pwr.edu.pl>
2021-03-27 12:07:28 +0100

    Added main loop

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	std::cout << "Hello" << std::endl;
+	// creating window
+	unsigned int win_width { 800 }, win_height { 600 };
+	sf::RenderWindow window( sf::VideoMode( win_width, win_height ), "Particles demo" );
+	window.setVerticalSyncEnabled( true );
+
+	sf::Event event {};
+	// main loop
+	while (window.isOpen())
+	{
+		// event loop
+		while ( window.pollEvent(event) )
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		// rendering stuff
+		window.clear( sf::Color::Black );
+		// window.draw();
+		window.display();
+	}
+
+	return 0;
 }
